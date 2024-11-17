6de638016d0526f9d3d1093ae8d2a7240f712077
JDog1925 <Jordallypaulino1819@gmail.com>
2024-11-17 18:12:00 -0500

    Add files via upload

---
+++ Assiment 1.py
@@ -0,0 +1,15 @@
+
+# Input display the 3 values
+principal = float(input("enter the principal amount: "))
+
+rate = float(input("enter the interest rate in percentage: "))
+
+time = float(input("enter the time period in years: "))
+
+# Calculate the simple interest
+
+simpleInterest = (principal * rate* time) / 100
+
+# Display the output
+
+print("The calculated simple interest is: ", simpleInterest)
