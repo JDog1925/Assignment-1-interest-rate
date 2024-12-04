2cfa988665db78d18b95228c538917749cbb714d
JDog1925 <Jordallypaulino1819@gmail.com>
2024-12-03 20:02:10 -0500

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,12 @@
+# Input display the 2 values
+Weight = float(input("enter Weight in kilograms: "))
+
+Height = float(input("enter Height in meters: "))
+
+# Calculate the BMI
+
+BMI = (Weight/(Height**2))
+
+# Display the output
+
+print("The calculated BMI is: ", BMI)
